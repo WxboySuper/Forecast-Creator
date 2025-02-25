1f6b41006dfef54a2e797e48a15474dc1be82e08
WeatherboySuper <SuperPlays1020@gmail.com>
2025-02-25 11:01:31 -0600

    fix: Correct spelling of 'canExecute' and improve null handling in RelayCommand; update project file for WPF support

--- src/ForecastCreator.Core/Base/RelayCommand.cs
+++ src/ForecastCreator.Core/Base/RelayCommand.cs
@@ -5,22 +5,28 @@ namespace ForecastCreator.Core.Base
     public class RelayCommand : ICommand
     {
         private readonly Action<object?> _execute;
-        private readonly Func<object?, bool>? _canExeecute;
+        private readonly Func<object?, bool>? _canExecute;
 
         public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
         {
-            _execute = execute;
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
             _canExecute = canExecute;
         }
 
         public event EventHandler? CanExecuteChanged
         {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
         }
 
-        public bool CanExecute(object? parameter) => _canExeecute?.Invoke(parameter) ?? true;
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
 
-        public void Execute(object? parameter) => _execute(parameter);
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
     }
 }
