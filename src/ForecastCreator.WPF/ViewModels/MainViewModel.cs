5385321e2b7e95554d75e10c0f5a8629bb85276c
WeatherboySuper <SuperPlays1020@gmail.com>
2025-02-24 20:09:39 -0600

    feat: Implement MVVM base classes with ObservableObject and RelayCommand

---
+++ src/ForecastCreator.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+using ForecastCreator.Core.Base;
+
+namespace ForecastCreator.WPF.ViewModels
+{
+    public class MainViewModel : ObservableOBject
+    {
+        private string _title = "Forecast Creator";
+        public string Title
+        {
+            get => _title;
+            set => SetField(ref _title, value);
+        }
+    }
+}
